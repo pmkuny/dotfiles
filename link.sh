cd3ee677a8e05811bd18a7d5bedd121e54f33ffa
Patrick Kuny <pmk62810@gmail.com>
2019-04-03 18:08:43 -0500

    Add inputrc and more vim-like tmux settings

--- link.sh
+++ link.sh
@@ -5,5 +5,6 @@ ln -s $PWD/bashrc $HOME/.bashrc
 ln -s $PWD/gitconfig $HOME/.gitconfig
 mkdir ~/.vim
 ln -s $PWD/vimrc $HOME/.vim/vimrc
+ln -s $PWD/inputrc $HOME/.inputrc
 ln -s $PWD/tmux.conf $HOME/.tmux.conf
 echo "[INFO] DONE"
