2a03d76fcafcf7f9503e4e739efc44783c4bbe85
Patrick Kuny <31052319+pmkuny@users.noreply.github.com>
2025-07-03 23:40:43 -0500

    Update link.sh

--- link.sh
+++ link.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
-echo "[INFO] Linking files.."
+ln -s $PWD/zshrc $HOME/.zshrc
 ln -s $PWD/bashrc $HOME/.bashrc
 ln -s $PWD/gitconfig $HOME/.gitconfig
 mkdir ~/.vim
 ln -s $PWD/vimrc $HOME/.vim/vimrc
 ln -s $PWD/inputrc $HOME/.inputrc
 ln -s $PWD/tmux.conf $HOME/.tmux.conf
-echo "[INFO] DONE"
+
